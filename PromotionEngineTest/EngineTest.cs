23e9245d6e88503822b2cb1dee00cff51afb517d
abdss <abdulshaheensha@gmail.com>
2021-08-09 21:48:26 +0530

    renamed classes

--- PromotionEngineTest/EngineTest.cs
+++ PromotionEngineTest/EngineTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PromotionEngineTest
 {
     [TestClass]
-    public class UnitTest1
+    public class EngineTest
     {
         [TestMethod]
         public void TestMethod1()
