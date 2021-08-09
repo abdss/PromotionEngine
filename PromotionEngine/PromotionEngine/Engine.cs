fd9d16d7e598cb7c9e31edd3352448744581c85a
abdss <abdulshaheensha@gmail.com>
2021-08-09 22:12:58 +0530

    added basic skeleton

--- PromotionEngine/PromotionEngine/Engine.cs
+++ PromotionEngine/PromotionEngine/Engine.cs
@@ -4,5 +4,29 @@ namespace PromotionEngine
 {
     public class Engine
     {
+        var priceA = 50;
+        var priceB = 30;
+        var priceC = 20;
+        var priceD = 15;
+
+        var promoQuantityA = 3;
+        var promoQuantityB = 2;
+
+        var promoPriceA = 130;
+        var promoPriceB = 45;
+        var promoPriceCD = 30;
+
+        public int CalculateOrderValue(int quantityA, int quantityB, int quantityC, int quantityD)
+        {
+            var total = 0;
+
+            total += ApplyPromotion(quantityA);
+
+            total += ApplyPromotion(quantityA);
+
+            total += ApplyCDPromotion(quantityC, quantityD);
+            
+            return total;
+        }
     }
 }
